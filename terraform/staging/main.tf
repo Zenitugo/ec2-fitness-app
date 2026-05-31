1778182c5aaa7c04a38af8c125e310f1f847e0b5
zenitugo <ugochiukaegbu19@gmail.com>
2026-05-31 15:57:19 +0100

    changed cidr block variable name

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1,6 +1,6 @@
 #################### VPC Modules ####################
 module "vpc" {
   source                              = "../modules/vpc"
-  cidr_block                          = var.cidr_block
+  vpc_cidr_block                      = var.vpc_cidr_block
   environment_name                    = var.environment_name
 }
