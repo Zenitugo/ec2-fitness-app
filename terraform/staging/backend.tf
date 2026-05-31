7e09a87a079f1b025b91bab2795a54e7c09cc0b7
zenitugo <ugochiukaegbu19@gmail.com>
2026-05-31 14:50:42 +0100

    fixed backend.tf

--- terraform/staging/backend.tf
+++ terraform/staging/backend.tf
@@ -1,9 +1,9 @@
 terraform {
     backend "s3" { 
-        bucket = "ugochi-project1"
+        bucket = "ugochi-project1-buck"
         key     = "fitness-app/terraform.tfstate"
         dynamodb_table = "ugochi-fitness-app-lock"
-        region = var.region
+        region = "eu-central-1"
         encrypt = true
         use_lockfile = true
     }
