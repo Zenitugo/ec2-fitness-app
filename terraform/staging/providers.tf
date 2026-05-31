5433a2f0e5cb2f5c14533c0aa484bae6c91920c7
zenitugo <ugochiukaegbu19@gmail.com>
2026-05-31 14:51:03 +0100

    fixed providers.tf

--- terraform/staging/providers.tf
+++ terraform/staging/providers.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "6.11.0"
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = var.region
+  region = "eu-central-1"
 }
