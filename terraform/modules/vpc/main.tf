a190709a1cb4d2c97a5c05004d5a0514cc18195d
zenitugo <ugochiukaegbu19@gmail.com>
2026-05-31 11:37:52 +0100

    cnfiguration to asscociate subnets with route tables

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -148,3 +148,25 @@ resource "aws_route" "private_route_2" {
   destination_cidr_block = "0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat_gw_2.id
 }
+
+
+########################### Associate Subnets with Route Tables  #############################
+resource "aws_route_table_association" "public_subnet_1_assoc" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_assoc" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+} 
+
+resource "aws_route_table_association" "private_subnet_1_assoc" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_rt_1.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_assoc" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt_2.id
+}
