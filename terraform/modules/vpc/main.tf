5030c5cbc8c702974f386ff090891be27108424e
zenitugo <ugochiukaegbu19@gmail.com>
2026-05-31 11:24:57 +0100

    configuration to create nat gateways

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -84,3 +84,22 @@ resource "aws_eip" "nat_eip_2" {
   }
 }
 
+
+
+########################## Create NAT Gateways  #############################
+resource "aws_nat_gateway" "nat_gw_1" {
+  allocation_id = aws_eip.nat_eip_1.id
+  subnet_id     = aws_subnet.public_subnet_1.id    
+    tags = {
+        Name = "${var.environment_name}-nat-gw-1"
+    }
+}
+
+
+resource "aws_nat_gateway" "nat_gw_2" {
+  allocation_id = aws_eip.nat_eip_2.id
+  subnet_id     = aws_subnet.public_subnet_2.id    
+    tags = {
+        Name = "${var.environment_name}-nat-gw-2"
+    }
+}
