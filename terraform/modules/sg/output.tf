9a8d558a5d1dcfcac26ff8a7216bdcce2a8911c7
zenitugo <ugochiukaegbu19@gmail.com>
2026-06-07 23:55:24 +0100

    sg ids exported

---
+++ terraform/modules/sg/output.tf
@@ -0,0 +1,17 @@
+output alb_sg  {
+  value       = aws_security_group.alb_sg.id
+}
+
+
+output frontend_sg  {
+  value       = aws_security_group.frontend_sg.id
+}
+
+
+output backend_sg  {
+  value       = aws_security_group.backend_sg.id
+}
+
+output database_sg  {
+  value       = aws_security_group.database_sg.id
+}
