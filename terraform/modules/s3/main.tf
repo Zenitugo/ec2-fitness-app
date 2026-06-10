f2212269360eb5d140413da69251b4bf4d0e9ea8
zenitugo <ugochiukaegbu19@gmail.com>
2026-06-10 07:27:23 +0100

    configuration to create bucket to serve static files and  upload media

---
+++ terraform/modules/s3/main.tf
@@ -0,0 +1,52 @@
+############################## Create s3 Bucket for storing media photos ##############################
+
+  # Media Bucket (Fully Private)
+resource "aws_s3_bucket" "media_bucket" {
+  bucket_prefix = var.bucket_prefix
+  region        = var.region
+  private      = true
+}
+
+resource "aws_s3_bucket_public_access_block" "media_bucket_privacy" {
+  bucket = aws_s3_bucket.media_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+
+########################## Crreate S3 Bucket for Frontend Hosting (Public) ##########################
+# Frontend Bucket
+resource "aws_s3_bucket" "frontend_bucket" {
+  bucket_prefix = "${var.bucket_prefix}-frontend"
+}
+
+# Static Website Hosting Config
+resource "aws_s3_bucket_website_configuration" "frontend_hosting" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
+
+# Strict Block Public Access (Frontend served via CloudFront only)
+resource "aws_s3_bucket_public_access_block" "frontend_privacy" {
+  bucket = aws_s3_bucket.frontend_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+
+
