eec435b7339739fd18bd8a050044e5f253d960b4
zenitugo <ugochiukaegbu19@gmail.com>
2026-06-10 07:28:01 +0100

    export bucket arn and domain name

---
+++ terraform/modules/s3/output.tf
@@ -0,0 +1,18 @@
+#################### Export IDs ############################
+
+output "bucket_arn" {
+  value = aws_s3_bucket.media_bucket.arn
+}
+
+
+output  "bucket_regional_domain_name" {
+  value = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
+}
+
+output "frontend_bucket_name" {
+  value = aws_s3_bucket.frontend_bucket.id
+}
+
+output "frontend_bucket_arn" {
+  value = aws_s3_bucket.frontend_bucket.arn
+}
