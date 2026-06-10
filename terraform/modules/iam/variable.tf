1d0304405fe8780090eab305d22dadd90b156c31
zenitugo <ugochiukaegbu19@gmail.com>
2026-06-10 07:22:13 +0100

    added more variables declared in iam resource

--- terraform/modules/iam/variable.tf
+++ terraform/modules/iam/variable.tf
@@ -1,3 +1,7 @@
 variable "Instance_profile_name" {}
 variable "role_name" {}
 variable "custom_policy_name" {}
+variable "bucket_arn" {}
+variable "frontend_bucket_arn" {}
+variable "frontend_bucket_name" {}
+variable "cloudfront_distribution_arn" {}
