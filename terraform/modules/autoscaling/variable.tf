0f8abc7307e52f2ff231d5c34fd774e8645a9325
zenitugo <ugochiukaegbu19@gmail.com>
2026-06-17 07:00:22 +0100

    variables declared in autoscaling group resource

---
+++ terraform/modules/autoscaling/variable.tf
@@ -0,0 +1,7 @@
+variable private_subnet_1_id {}
+variable private_subnet_2_id {}
+variable min_size {}
+variable max_size {}
+variable desired_capacity {}
+variable environment_name {}
+variable launch_template {}
