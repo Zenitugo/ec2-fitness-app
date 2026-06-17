09a8d62fd3404a6db15c3924e567c2f392d463b8
zenitugo <ugochiukaegbu19@gmail.com>
2026-06-17 06:09:36 +0100

    configuration to create a launch template for ec2 server

---
+++ terraform/modules/ec2-template/main.tf
@@ -0,0 +1,21 @@
+#######################  Launch an Instance Template #######################
+resource "aws_launch_template" "fitness_app_launch_template" {
+  name_prefix   = "${var.environment_name}-fitness-app"
+  image_id           = var.image_id
+  instance_type = var.instance_type
+  key_name      = data.aws_key_pair.key.key_name
+  vpc_security_group_ids = [var.ec2_sg]
+  iam_instance_profile {
+    name = var.Instance_profile_name
+  }
+  
+  # Enforce IMDSV2 for security
+  metadata_options {
+    http_tokens = "required"
+    http_endpoint = "enabled"
+    http_put_response_hop_limit = 1
+    instance_metadata_tags = "disabled"
+  }
+
+  user_data = file("${path.module}/template.sh")
+}
