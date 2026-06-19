ebf29eae6a2734aa4e88599bd9822f3e4c55a079
zenitugo <ugochiukaegbu19@gmail.com>
2026-06-19 06:51:05 +0100

    changed fitness to fittrack

--- terraform/modules/rds/output.tf
+++ terraform/modules/rds/output.tf
@@ -1,5 +1,5 @@
 # Reference the auto generated secret ARN
 output "db_secret_arn" {
-  value       = aws_db_instance.fitness_rds.master_user_secret[0].secret_arn
+  value       = aws_db_instance.fittrack_rds.master_user_secret[0].secret_arn
   description = "ARN of the RDS managed secret"
 }
