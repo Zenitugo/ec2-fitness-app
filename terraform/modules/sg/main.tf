6ea086bc49df43d4b8bd6221fb7f4463ec7c1698
zenitugo <ugochiukaegbu19@gmail.com>
2026-06-19 06:46:17 +0100

    fixed vpc declaration

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -2,7 +2,7 @@
 resource "aws_security_group" "alb_sg" {
   name        = "${var.environment_name}-alb-sg"
   description = "Security group for the ALB"
-  vpc_id      = aws_vpc.vpc.id 
+  vpc_id      = var.vpc_id 
 
     ingress {
         description      = "Allow HTTP traffic"
