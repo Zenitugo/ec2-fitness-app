7cba2280cd8e2f5d9b4bf09c097e3035c0057388
zenitugo <ugochiukaegbu19@gmail.com>
2026-06-19 04:22:19 +0100

    fixed i in Instance profile name to I

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -34,7 +34,7 @@ resource "aws_iam_role_policy_attachment" "cloudwatch_attach" {
 
 ##################### Create IAM Instance Profile #####################
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
-  name = var.instance_profile_name
+  name = var.Instance_profile_name
   role = aws_iam_role.ec2_role.name
 }
 
