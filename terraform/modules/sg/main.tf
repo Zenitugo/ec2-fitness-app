4e048d1d8847cb43da569f541aa30f287bb1af67
zenitugo <ugochiukaegbu19@gmail.com>
2026-06-19 07:06:54 +0100

    removed frontend sg because it will be served through s3 and cloud front

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -23,30 +23,6 @@ resource "aws_security_group" "alb_sg" {
 
 
 
-
-resource "aws_security_group" "frontend_sg" {
-    name        = "${var.environment_name}-frontend-sg"
-    description = "Security group for the frontend servers"
-    vpc_id      = var.vpc_id
-
-    ingress {
-        description      = "Allow HTTP traffic from ALB"
-        from_port        = 3000
-        to_port          = 3000
-        protocol         = "tcp"
-        security_groups  = [aws_security_group.alb_sg.id]
-    }
-
-    egress {
-        description      = "Allow all outbound traffic"
-        from_port        = 0
-        to_port          = 0
-        protocol         = "-1"
-        cidr_blocks      = ["0.0.0.0/0"]
-    }
-}
-
-
 resource "aws_security_group" "backend_sg" {
     name        = "${var.environment_name}-backend-sg"
     description = "Security group for the backend servers"
