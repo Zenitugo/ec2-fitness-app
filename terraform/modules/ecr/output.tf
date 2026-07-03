dd378666772d62b5e4356c36cb9fe9729bb63101
zenitugo <ugochiukaegbu19@gmail.com>
2026-07-03 06:53:08 +0100

    removed configuration to export front end repo url

--- terraform/modules/ecr/output.tf
+++ terraform/modules/ecr/output.tf
@@ -1,9 +1,5 @@
 #################### Export ECR Repository ID ####################
 
-output "frontend_repository_id" {
-    value = aws_ecr_repository.frontend_repository.id
-}
-
-output "backend_repository_id" {
-    value = aws_ecr_repository.backend_repository.id
+output "backend_repository_url" {
+    value = aws_ecr_repository.backend_repository.repository_url
 }
