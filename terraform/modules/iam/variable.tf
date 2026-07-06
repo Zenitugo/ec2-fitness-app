4a4fb4878d714eaedf3be1fdc64c54074b6133ee
zenitugo <ugochiukaegbu19@gmail.com>
2026-07-06 10:12:23 +0100

    commented put the cloudfront variable

--- terraform/modules/iam/variable.tf
+++ terraform/modules/iam/variable.tf
@@ -4,4 +4,4 @@ variable "custom_policy_name" {}
 variable "bucket_arn" {}
 variable "frontend_bucket_arn" {}
 variable "frontend_bucket_name" {}
-variable "cloudfront_distribution_arn" {}
+# variable "cloudfront_distribution_arn" {}
