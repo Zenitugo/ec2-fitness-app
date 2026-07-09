920bbcb5b579dce1252023e7edfec01265d3dfd2
zenitugo <ugochiukaegbu19@gmail.com>
2026-07-09 13:58:27 +0100

    export load balancer dns name

--- terraform/modules/alb/output.tf
+++ terraform/modules/alb/output.tf
@@ -3,3 +3,7 @@ output "target_group_arn" {
   description = "The ARN of the Target Group"
   value       = aws_lb_target_group.fittrack_app_target_group.arn
 }
+
+output "alb_dns_name" {
+  value   = aws_lb.fittrack_load_balancer.dns_name
+}
