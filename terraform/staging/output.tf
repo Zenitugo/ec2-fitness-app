90d056830f992540ca99a28f17040d7cba24df93
zenitugo <ugochiukaegbu19@gmail.com>
2026-07-10 17:46:47 +0100

    included configuration to export alb dnsname and s3 bucket name

--- terraform/staging/output.tf
+++ terraform/staging/output.tf
@@ -3,3 +3,13 @@
 output "backend_repository_url" {
     value = module.ecr.backend_repository_url
 }
+
+######################## Output DNS Name ################################
+output "alb_dns_name" {
+  value   = module.alb.alb_dns_name
+}
+
+###################### Export the Bucket Name holding the Frontend Assets ##################
+output "frontend_bucket_name" {
+  value = module.s3.frontend_bucket_name
+}
