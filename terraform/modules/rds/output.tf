f276af0f22e063b45df6c1c98375fac9e296b036
zenitugo <ugochiukaegbu19@gmail.com>
2026-07-11 18:32:46 +0100

    exporting db endpoint

--- terraform/modules/rds/output.tf
+++ terraform/modules/rds/output.tf
@@ -3,3 +3,9 @@ output "db_secret_arn" {
   value       = aws_db_instance.fittrack_rds.master_user_secret[0].secret_arn
   description = "ARN of the RDS managed secret"
 }
+
+
+#Export the rds endpoint
+output "db_endpoint" {
+  value = aws_db_instance.fittrack_rds.address
+}
