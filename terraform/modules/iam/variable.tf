f35ec9e57dbc6b6daf7bea258fc3408ce6a0422f
zenitugo <ugochiukaegbu19@gmail.com>
2026-07-18 13:54:25 +0100

    included secret arn variable

--- terraform/modules/iam/variable.tf
+++ terraform/modules/iam/variable.tf
@@ -4,4 +4,5 @@ variable "custom_policy_name" {}
 variable "bucket_arn" {}
 variable "frontend_bucket_arn" {}
 variable "frontend_bucket_name" {}
+variable "db_secret_arn" {}
 # variable "cloudfront_distribution_arn" {}
