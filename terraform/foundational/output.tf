527e37588528e301f6998f0916ef439688e9cc63
zenitugo <ugochiukaegbu19@gmail.com>
2026-07-29 16:14:00 +0100

    foundational infrastructure

---
+++ terraform/foundational/output.tf
@@ -0,0 +1,81 @@
+#################### Export ECR Repository ID ####################
+
+output "backend_repository_url" {
+    value = module.ecr.backend_repository_url
+}
+
+######################## Output DNS Name ################################
+output "alb_dns_name" {
+  value   = module.alb.alb_dns_name
+}
+
+###################### Export the Bucket Name holding the Frontend Assets ##################
+output "frontend_bucket_name" {
+  value = module.s3.frontend_bucket_name
+}
+
+
+##############   Export the RDS ENdpoint #########################
+output "db_endpoint" {
+  value = module.rds.db_endpoint
+}
+
+
+################ Export Secret ARN ###########################
+output "db_secret_arn" {
+  value = module.rds.db_secret_arn
+  sensitive = true
+}
+
+
+############### Export Media Bucket ######################
+
+output "media_bucket_id" {
+  value = module.s3.media_bucket_id
+}
+
+
+####################### Export Backend Sg ####################
+output "backend_sg" {
+  value = module.sg.backend_sg
+}
+
+
+################## Export Alb Sg #####################
+output "alb_sg" {
+  value = module.sg.alb_sg
+}
+
+############## Export Instance Profile Name ##################
+output "instance_profile_name" {
+  value = module.iam.instance_profile_name
+}
+
+
+################ Export VPC ID ################
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+
+############### Export Public subnet 1 ID ###############
+output "public_subnet_1_id" {
+  value = module.vpc.public_subnet_1_id
+}
+
+
+############### Export Public subnet 2 ID ###############
+
+output "public_subnet_2_id" {
+  value = module.vpc.public_subnet_2_id
+}
+
+############### Export Private subnet 1 ID ###############
+output "private_subnet_1_id" {
+  value = module.vpc.private_subnet_1_id
+}
+
+############### Export Private subnet 2 ID ###############
+output "private_subnet_2_id" {
+  value = module.vpc.private_subnet_2_id
+}
