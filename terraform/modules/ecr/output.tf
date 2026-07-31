f560ad5df4031c269d81486c062963ef31ee4707
zenitugo <ugochiukaegbu19@gmail.com>
2026-07-31 15:57:05 +0100

    extract repo name

--- terraform/modules/ecr/output.tf
+++ terraform/modules/ecr/output.tf
@@ -3,3 +3,8 @@
 output "backend_repository_url" {
     value = aws_ecr_repository.backend_repository.repository_url
 }
+
+
+output "backend_repo_name" {
+    value = aws_ecr_repository.backend_repository.name
+}
