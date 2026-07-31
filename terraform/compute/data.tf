a7f673f9200aa298fd7d383913aba079ca384c6c
zenitugo <ugochiukaegbu19@gmail.com>
2026-07-31 09:48:02 +0100

    fixed key name

--- terraform/compute/data.tf
+++ terraform/compute/data.tf
@@ -3,7 +3,7 @@ data "terraform_remote_state" "foundation" {
   backend = "s3"
   config = {
     bucket = "ugochi-project1-buck"
-    key    = "foundation/terraform.tfstate"
+    key    = "fitness-app/terraform.tfstate"
     region = "eu-central-1"
   }
 }
