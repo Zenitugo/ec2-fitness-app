fcb97ce7ee1725af98fa74e703948ec8bf9031a2
zenitugo <ugochiukaegbu19@gmail.com>
2026-08-01 16:34:27 +0100

    exported media bucket name

--- terraform/modules/s3/output.tf
+++ terraform/modules/s3/output.tf
@@ -4,6 +4,10 @@ output "bucket_arn" {
   value = aws_s3_bucket.media_bucket.arn
 }
 
+output "bucket_name" {
+  value = aws_s3_bucket.media_bucket.id
+}
+
 
 output  "bucket_regional_domain_name" {
   value = aws_s3_bucket.frontend_bucket.bucket_regional_domain_name
