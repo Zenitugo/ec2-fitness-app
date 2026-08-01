554b86a4c5e0967c6344fea0bc24bad0be71328c
zenitugo <ugochiukaegbu19@gmail.com>
2026-08-01 13:59:16 +0100

    no need for keys using ssm manager

--- terraform/modules/ec2-template/main.tf
+++ terraform/modules/ec2-template/main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "fitness_app_launch_template" {
   name_prefix   = "${var.environment_name}-fitness-app"
   image_id           = var.image_id
   instance_type = var.instance_type
-  key_name      = data.aws_key_pair.key.key_name
+  #key_name      = data.aws_key_pair.key.key_name
   vpc_security_group_ids = [var.backend_sg]
 
   iam_instance_profile {
